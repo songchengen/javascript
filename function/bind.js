29f2fb0ec06f8c97fb6384a62d84872002e1bdab
songchengen <songchengen@T4F-MAM-14191.local>
2018-07-18 15:18:56 +0800

    【修改】function ——> bind

--- function/bind.js
+++ function/bind.js
@@ -9,8 +9,8 @@
  * 用ES5实现bind
  * 1、基本的绑定功能
  * 2、使用new关键字时绑定this无效，参数生效
- * @param {Function} func
- * @param {Object} ctx
+ * @param {Function} func 需要绑定的函数
+ * @param {Object} ctx 需要绑定的this对象
  * @returns {Function}
  */
 module.exports = function (func, ctx) {
@@ -22,9 +22,20 @@ module.exports = function (func, ctx) {
 
   const args = [].slice.call(arguments, 2);
 
+  /**
+   * 构造函数生成对象实例
+   * @returns {Object|*}
+   */
   const factory = function () {
     const obj = {};
+
+    /* 设置原型指向 */
     obj.__proto__ = this.prototype;
+
+    /**
+     * 1、执行目标函数，绑定函数内部的属性
+     * 2、如果目标函数有对象类型的返回值则取返回值，符合js new关键字的规范
+     */
     const ret = this.apply(obj, arguments);
     return typeof ret === 'object' ? ret : obj;
   };
